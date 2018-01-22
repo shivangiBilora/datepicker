520a9679ef56f43674ad9e2014213849d68ee822
shivangibilora <shivangibilora@gmail.com>
2018-01-22 15:30:27 +0530

    clean: code

--- app.js
+++ app.js
@@ -1,34 +1,79 @@
 (function() {
-    console.log("texting");
 
     let today = new Date();
-    let monthsWith31Days = [0, 2, 4, 6, 7, 9, 11];
-    let monthsWith30Days = [1, 3, 5, 8, 10];
-    let months = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ];
-    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-    let activeMonth = '';
-    let activeDate = '';
-    let calendarDateEle = $(".selected-date .date");
-    calendarDateEle.text(today.getDate());
-    let calendarDayEle = $(".selected-date .day");
-    calendarDayEle.text(days[today.getDay() - 1]);
+    let monthsWith31Days, monthsWith30Days, months, days;
+    let activeMonth, activeDate = '';
+
+    let calendarDateEle, calendarDayEle;
 
 
+
+    /**
+     *
+     * @method to initilise all the constants in the calendar
+     *
+     */
+    let initConstants = function() {
+
+        monthsWith31Days = [0, 2, 4, 6, 7, 9, 11];
+        monthsWith30Days = [3, 5, 8, 10];
+        months = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+    };
+
+
+
+    /**
+     *
+     * @method to initilise all the elements required for manipulating content
+     *
+     */
+
+    let initElements = function() {
+        calendarDateEle = $(".selected-date .date");
+        calendarDayEle = $(".selected-date .day");
+    };
+
+
+
+    /**
+     *
+     * @method to initilise all the elements to the initial state, ie today
+     *
+     */
+    let initTodaysDate = function() {
+        let date = today.getDate();
+        let day = today.getDay();
+
+        calendarDateEle.text(date);
+        calendarDayEle.text(days[day - 1]);
+
+        $($('.week-dates').find('h4')[date - 1]).addClass('active');
+
+    };
+
+
+    /**
+     *
+     * @method to generate calendar
+     *
+     */
     let generateDateElements = function(date) {
 
         let month = date.getMonth();
         let year = date.getFullYear();
         let totalDays = monthsWith31Days.includes(month) ? 31 :
             (monthsWith30Days.includes(month) ? 30 : (year % 4 === 0 ? 29 : 28));
-        let selDate = date.getDate();
 
         activeMonth = month;
         activeDate = date;
 
-        let firstDate = new Date(date.setDate(1));
+        let firstDate = new Date(date.getTime());
+        firstDate.setDate(1);
 
         let firstDay = firstDate.getDay();
         let dateElements = '<div class="week">';
@@ -58,10 +103,19 @@
         $('.week-dates').append(dateElements);
 
         $('.month-name').text(`${months[month]} ${year}`);
-        $($('.week-dates').find('h4')[selDate - 1]).addClass('active');
+
+        if (month === today.getMonth() - 1) {
+            initTodaysDate();
+        }
     };
 
 
+
+    /**
+     *
+     * @method to generate calendar when next is clicked
+     *
+     */
     $(".showNextMonth").on('click', function() {
         let currDate = activeDate;
         currDate.setMonth(activeMonth + 1);
@@ -69,6 +123,12 @@
         generateDateElements(currDate)
     });
 
+
+    /**
+     *
+     * @method to generate calendar when previous is clicked
+     *
+     */
     $(".showPrevMonth").on('click', function() {
         let currDate = activeDate;
         currDate.setMonth(activeMonth - 1);
@@ -76,17 +136,31 @@
         generateDateElements(currDate)
     });
 
+
+    /**
+     *
+     * @method to show current clicked date on right side calendar
+     *
+     */
     $(".week-dates").on('click', 'h4', function(evt) {
+
         let eleIndex = Array.prototype.indexOf.call(this.parentElement.children, this);
-        $(evt.target).closest('.week-dates').find('h4').removeClass('active');
-        $(evt.target).addClass('active');
-        calendarDateEle.text(evt.target.textContent);
+        let target = evt.target;
+
+        $(target).closest('.week-dates').find('h4').removeClass('active');
+        $(target).addClass('active');
+
+        calendarDateEle.text(target.textContent);
         calendarDayEle.text(days[eleIndex]);
 
-        (eleIndex % 6 === 0) ? $(".selected-date .inner-container").addClass("weekend"):
+        ((eleIndex + 1) % 7 === 0) ? $(".selected-date .inner-container").addClass("weekend"):
             $(".selected-date .inner-container").removeClass("weekend");
     });
 
+    initConstants();
     generateDateElements(today);
+
+    initElements();
+    initTodaysDate();
 
 })();
